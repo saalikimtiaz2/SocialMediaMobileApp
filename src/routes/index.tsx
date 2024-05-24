c547d70ac60be88eef49060a7b57fc8a85fa526b
Salik Imtiaz <r.salikimtiaz@gmail.com>
2024-05-25 00:29:46 +0400

    header removed

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ import Messages from '@/screens/Messages';
 function Router(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Messages" component={Messages} />
       </Stack.Navigator>
